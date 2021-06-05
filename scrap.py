32115264c425a4d641defcb1ab29c6912db0523c
shruti-santosh <shrutisantosh@yahoo.com>
2021-06-05 13:46:11 +0530

    add code (see details)
    added code that doesnt work but i have changed the id and classes for the search criteria

--- scrap.py
+++ scrap.py
@@ -5,9 +5,10 @@ URL = 'https://internshala.com/internships-for-women/matching-preferences'
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content,'html.parser')
-results = soup.find(id='internship_list_container')
-interships = results.find_all('div',class_ = 'internship-list-container')
-for individual_intern in interships:
-	title_elem = individual_intern.find('div', class_ = "heading_4_5 profile")
-	print(title_elem.text)
-	print()
+results = soup.find(id='list_container')
+interships = results.find_all('div', {"id" : "internship-list-container"})
+print(interships)
+for individual_intern in interships[1:]:
+	print("in this loop")
+	title_elem = individual_intern.find('div', {"class" : "individual_internship_header"})
+	print("title is", title_elem)
